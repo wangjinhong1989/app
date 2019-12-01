1fc21abfd8f6c6096cbb0663f996e733809f993b
wangjinhong1989 <392513170@qq.com>
2019-12-01 11:33:46 +0800

    ad

---
+++ application/api/controller/Ad.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace app\api\controller;
+
+use app\common\controller\Api;
+use app\admin\model\Guanggao;
+
+/**
+ * 首页接口
+ */
+class Ad extends Api
+{
+    protected $noNeedLogin = ['*'];
+    protected $noNeedRight = ['*'];
+
+    /**
+     * 首页
+     *
+     */
+    public function Lists()
+    {
+        $lists=(new Guanggao())->where(['status'=>0])->all();
+        $this->success($lists);
+    }
+}
