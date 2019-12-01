95ff5f6549530d94e14bf5395bab6b7455daa17d
wangjinhong1989 <392513170@qq.com>
2019-12-01 15:05:29 +0800

    ad

--- application/api/controller/Report.php
+++ application/api/controller/Report.php
@@ -41,6 +41,9 @@ class Report extends Api
 
         var_dump($this->request->request());
         var_dump($this->request->request('user_id'));
+        var_dump($this->request->post());
+        var_dump($_POST);
+        var_dump($_POST['user_id']);
         if(!$user_id||!$type||!$article_id||!$content){
             $this->error(__('参数存在空'));
         }
