a49821c93250cb1be77c8c04eb00fe5f70fddb7c
wangjinhong1989 <392513170@qq.com>
2019-12-01 18:54:37 +0800

    jubao

---
+++ application/api/controller/HistorySearch.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace app\api\controller;
+
+
+use app\admin\model\SearchHistory;
+use app\common\controller\Api;
+/**
+ * 首页接口
+ */
+class HistorySearch extends Api
+{
+    protected $noNeedLogin = [];
+    protected $noNeedRight = ['*'];
+
+    /**
+     * 热搜
+     *
+     */
+    public function Lists()
+    {
+
+        $lists=( new SearchHistory())->where(['user_id'=>$this->auth->getUser()->id])->select();
+        $this->success($lists);
+    }
+
+
+
+
+    /*
+    *添加收藏
+    * **/
+    public function add()
+    {
+
+        try{
+            $data=[];
+            $model=new SearchHistory();
+            $user = $this->auth->getUser();
+            $user_id = $user->id;
+            $content=$this->request->request('word');
+
+            if(!$content){
+                return $this->error(__('参数存在空'));die;
+            }
+
+            $model->create([
+                'user_id'=>$user_id,'word'=>$content,'time'=>time()
+            ]);
+
+            return $this->success('123',$model->getLastSql());
+        }catch (Exception $e){
+            return  $this->error($e->getMessage());
+        }
+
+    }
+}
