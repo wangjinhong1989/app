a4240b1beae80ca2d868b3d765a823a67c47fb63
wangjinhong1989 <392513170@qq.com>
2019-12-01 14:22:26 +0800

    ad

--- application/api/controller/Report.php
+++ application/api/controller/Report.php
@@ -27,8 +27,8 @@ class Report extends Api
     {
 
         $model=new Jubao();
-        $model->insert($_POST);
-        $this->success('123',$_POST);
+        $model->data($this->request->post())->insert();
+        $this->success('123',$this->request->post());
 
     }
 }
