71091f0bc55b8e953fa4948b08391544d840f74c
wangjinhong1989 <392513170@qq.com>
2019-12-01 18:40:50 +0800

    type

---
+++ application/api/controller/ArticleTypeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace app\api\controller;
+
+
+use app\admin\model\Shoucang;
+use app\admin\model\Articletype;
+use app\common\controller\Api;
+/**
+ * 首页接口
+ */
+class ArticleTypeController extends Api
+{
+    protected $noNeedLogin = [];
+    protected $noNeedRight = ['*'];
+
+    /**
+     * 首页
+     *
+     */
+    public function Lists()
+    {
+        $lists=( new Articletype())->where(['status'=>0])->select();
+        $this->success($lists);
+    }
+
+}
