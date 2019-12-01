5b3b5a6e1cf78a4a92da32722845f1e484df22a7
wangjinhong1989 <392513170@qq.com>
2019-12-01 16:08:45 +0800

    jubao

---
+++ application/api/controller/ProblemPlatform.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace app\api\controller;
+
+
+use app\common\controller\Api;
+use app\admin\model\Problem;
+/**
+ * 首页接口
+ */
+class ProblemPlatform extends Api
+{
+    protected $noNeedLogin = [];
+    protected $noNeedRight = ['*'];
+
+    /**
+     * 首页
+     *
+     */
+    public function Lists()
+    {
+        $lists=(new Problem())->where(['status'=>0])->select();
+        $this->success($lists);
+    }
+}
