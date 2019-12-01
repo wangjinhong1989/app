c21f6bbc56b6e571a4f1f70205a1ad88509bcfd1
wangjinhong1989 <392513170@qq.com>
2019-12-01 15:45:01 +0800

    ad

--- application/api/controller/Report.php
+++ application/api/controller/Report.php
@@ -61,9 +61,8 @@ class Report extends Api
             return $this->error(__('文章不存在'));
         }
 
-
-
             $model->create(['user_id'=>$user_id,'article_id'=>$article_id,'content'=>$content,'type'=>$type]);
+
             return $this->success('123',$model->getLastSql());
         }catch (Exception $e){
             return  $this->error($e->getMessage());
