571a86fa771256e88612502a23268d58721b1592
王金宏 <2709989114@qq.com>
2019-12-02 13:45:11 +0800

    redis test

--- application/api/controller/Ad.php
+++ application/api/controller/Ad.php
@@ -4,7 +4,7 @@ namespace app\api\controller;
 
 use app\common\controller\Api;
 use app\admin\model\Guanggao;
-
+use think\Cache;
 /**
  * 首页接口
  */
@@ -19,7 +19,12 @@ class Ad extends Api
      */
     public function Lists()
     {
-        $lists=(new Guanggao())->where(['status'=>0])->select();
+        $lists=Cache::get('ad_list');
+        if(!$lists){
+            $lists=(new Guanggao())->where(['status'=>0])->select();
+            Cache::set('ad_list',$lists,0);
+        }
+
         $this->success($lists);
     }
 }
