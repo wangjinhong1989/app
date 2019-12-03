a057a90fd6dc834b31bf15b32f48e0e1d1cd6204
王金宏 <2709989114@qq.com>
2019-12-03 10:36:00 +0800

    add

---
+++ application/index/controller/Artilce.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace app\index\controller;
+
+use app\common\controller\Frontend;
+use app\common\library\Sms;
+use think\Config;
+use think\Cookie;
+use think\Hook;
+use think\Session;
+use think\Validate;
+
+/**
+ * 会员中心
+ */
+class Artilce extends Frontend
+{
+    protected $layout = 'default';
+    protected $noNeedLogin = [];
+    protected $noNeedRight = ['*'];
+
+    /**
+     * 文章列表
+     */
+    public function index()
+    {
+        $model=new \app\admin\model\Article();
+        $lists=$model->where(['user_id'=>$this->auth->getUser()->id])->select();
+        $this->view->assign('title', __(''));
+        var_dump($lists);
+        $this->view->assign('lists', $lists);
+        return $this->view->fetch();
+    }
+
+}
