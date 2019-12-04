fe2013c58d5b8454fbd99160daee59fc7f657208
wangjinhong1989 <392513170@qq.com>
2019-12-04 21:06:25 +0800

    1

--- application/admin/controller/Article.php
+++ application/admin/controller/Article.php
@@ -64,7 +64,8 @@ class Article extends Backend
 
             foreach ($list as $row) {
                 
-                
+                $row->getRelation('articletype')->visible(['name']);
+				$row->getRelation('user')->visible(['username']);
             }
             $list = collection($list)->toArray();
             $result = array("total" => $total, "rows" => $list);
