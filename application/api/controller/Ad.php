fe273cfc4aee5c8a8adb69b2f06b1c4f26991732
王金宏 <2709989114@qq.com>
2019-12-11 11:10:00 +0800

    1

--- application/api/controller/Ad.php
+++ application/api/controller/Ad.php
@@ -25,7 +25,7 @@ class Ad extends Api
         //Cache::store('redis')->clear();
         $lists=Cache::store('redis')->get('ad_list');
         if(!$lists){
-            $lists=(new Guanggao())->where(['status'=>'有效'])->select();
+            $lists=(new Guanggao())->where(['status'=>'显示'])->select();
             Cache::store('redis')->set('ad_list',$lists,60);
         }
 
