f3e49636f60bf20ede3f3b375ca4858671f84e6d
wangjinhong1989 <392513170@qq.com>
2019-12-15 16:48:47 +0800

    1

--- application/api/controller/Guanzhu.php
+++ application/api/controller/Guanzhu.php
@@ -29,6 +29,7 @@ class Guanzhu extends Api
         $model = (new \app\admin\model\Guanzhu());
         $lists = $model
             ->with(['user'])
+            ->field("guanzhu.*,user.nickname,user.avatar")
             ->where(['guanzhu.user_id' => $user_id])
             ->where('user.id=guanzhu.follow_id')
             ->select();
