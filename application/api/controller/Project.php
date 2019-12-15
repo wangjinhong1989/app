e9d5065d206766a1deb3bbc3edd8804624af1fed
wangjinhong1989 <392513170@qq.com>
2019-12-15 14:00:15 +0800

    1

--- application/api/controller/Project.php
+++ application/api/controller/Project.php
@@ -48,7 +48,6 @@ class Project extends Api
                 return $this->error(__('项目不存在'));
             }
             $info=$model::getById($id);
-
             $info->hot=$info->hot+1;
             $info->rank=$info->rank+1;
 //            $model->save(['hot'=>$info->hot,'rank'=>$info->rank],['id'=>$id]);
