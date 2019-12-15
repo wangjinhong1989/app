5dfe7b92314f7267fb2479a090d73eaaa455fb10
wangjinhong1989 <392513170@qq.com>
2019-12-15 11:28:51 +0800

    1

--- application/api/controller/MyCollect.php
+++ application/api/controller/MyCollect.php
@@ -22,7 +22,11 @@ class MyCollect extends Api
     {
         $user = $this->auth->getUser();
         $user_id=$user->id;
-        $lists=( new Shoucang())->alias("shoucang")->with(['article'])->where(['shoucang.user_id'=>$user_id])->where('article.id=shoucang.article_id')->select("shoucang.*,article.title");
+        $lists=( new Shoucang())->alias("shoucang")
+            ->with(['article'])
+            ->where(['shoucang.user_id'=>$user_id])
+//            ->where('article.id=shoucang.article_id')
+            ->select("shoucang.*,article.title");
         $this->success($lists);
     }
 
