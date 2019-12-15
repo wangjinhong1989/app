0f7fccd9e237c03875d0453f041ed0dd82e46939
wangjinhong1989 <392513170@qq.com>
2019-12-15 12:14:26 +0800

    1

--- application/api/controller/MyCollect.php
+++ application/api/controller/MyCollect.php
@@ -27,7 +27,7 @@ class MyCollect extends Api
 //            ->with(['article'])
        $lists= $model->alias('shoucang')
            ->field("shoucang.id,article.title,article.content,article.img,article.url,article.show_count,article.read_count,article.is_reply,article.is_mine")
-           ->with(['article','articletype'])
+           ->with(['article'])
             ->where(['shoucang.user_id'=>$user_id])
             ->where('article.id=shoucang.article_id')
             ->select();
