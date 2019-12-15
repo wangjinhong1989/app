60ab10aab8a491c3eafe99a9d4f46584c24fec86
wangjinhong1989 <392513170@qq.com>
2019-12-15 15:31:39 +0800

    1

--- application/api/controller/Feedback.php
+++ application/api/controller/Feedback.php
@@ -37,4 +37,34 @@ class Feedback extends Api
         $this->success("成功", $lists);
     }
 
+
+    /*
+    *添加反馈
+    * **/
+    public function add()
+    {
+
+        try{
+            $model=new \app\admin\model\Feedback();
+            $user = $this->auth->getUser();
+            $user_id = $user->id;
+            $feedback_type=$this->request->request('feedback_type');
+            $content=$this->request->request('content');
+            $files=$this->request->request('files');
+
+            if(!$content||!$files||$feedback_type){
+                return $this->error(__('参数存在空'));
+            }
+
+            $model->create([
+                'user_id'=>$user_id,'feedback_type'=>$feedback_type,'files'=>$files,'content'=>$content,'time'=>time()
+            ]);
+
+            return $this->success();
+        }catch (Exception $e){
+            return  $this->error($e->getMessage());
+        }
+
+    }
+
 }
