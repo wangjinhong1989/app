9525b786afedca37994c769dbed6430321a92438
王金宏 <2709989114@qq.com>
2019-12-20 17:38:24 +0800

    1

---
+++ application/api/controller/Qidong.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace app\api\controller;
+
+use app\common\controller\Api;
+use app\admin\model\Guanggao;
+use think\Cache;
+use think\Config;
+
+/**
+ * 首页接口
+ */
+class Qidong extends Api
+{
+    protected $noNeedLogin = ['*'];
+    protected $noNeedRight = ['*'];
+
+    /**
+     * 首页
+     *
+     */
+    public function Lists()
+    {
+            $lists=(new \app\admin\model\Qidong())->where([])->select();
+
+        $this->success("成功",$lists);
+    }
+}
