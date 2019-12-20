833366137a1a6d0acf7d56534eabd087f75ac71c
wangjinhong <392513170@qq.com>
2019-12-20 17:33:27 +0800

    1

---
+++ public/assets/js/backend/qidong.js
@@ -0,0 +1,51 @@
+define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefined, Backend, Table, Form) {
+
+    var Controller = {
+        index: function () {
+            // 初始化表格参数配置
+            Table.api.init({
+                extend: {
+                    index_url: 'qidong/index' + location.search,
+                    add_url: 'qidong/add',
+                    edit_url: 'qidong/edit',
+                    del_url: 'qidong/del',
+                    multi_url: 'qidong/multi',
+                    table: 'qidong',
+                }
+            });
+
+            var table = $("#table");
+
+            // 初始化表格
+            table.bootstrapTable({
+                url: $.fn.bootstrapTable.defaults.extend.index_url,
+                pk: 'id',
+                sortName: 'id',
+                columns: [
+                    [
+                        {checkbox: true},
+                        {field: 'id', title: __('Id')},
+                        {field: 'files', title: __('Files')},
+                        {field: 'url', title: __('Url'), formatter: Table.api.formatter.url},
+                        {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
+                    ]
+                ]
+            });
+
+            // 为表格绑定事件
+            Table.api.bindevent(table);
+        },
+        add: function () {
+            Controller.api.bindevent();
+        },
+        edit: function () {
+            Controller.api.bindevent();
+        },
+        api: {
+            bindevent: function () {
+                Form.api.bindevent($("form[role=form]"));
+            }
+        }
+    };
+    return Controller;
+});
