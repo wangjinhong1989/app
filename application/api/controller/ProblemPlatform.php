0f2d5b12b2908a50123b0dd4a40190a71058ee74
wangjinhong <392513170@qq.com>
2019-12-22 21:40:29 +0800

    1

--- application/api/controller/ProblemPlatform.php
+++ application/api/controller/ProblemPlatform.php
@@ -5,6 +5,8 @@ namespace app\api\controller;
 
 use app\common\controller\Api;
 use app\admin\model\Problem;
+use think\Request;
+
 /**
  * 首页接口
  */
@@ -20,6 +22,13 @@ class ProblemPlatform extends Api
     public function Lists()
     {
         $lists=(new Problem())->where(['status'=>'显示'])->select();
+        $this->success("成功",$lists);
+    }
+
+    public function detail()
+    {
+        $id=$this->request->request("id",0)
+        $lists=(new Problem())->where(['id'=>$id])->find();
         $this->success("成功",$lists);
     }
 
