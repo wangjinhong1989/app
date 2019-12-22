571abe8d77e2bce7f300e631ffbbffc668e2fe5a
wangjinhong <392513170@qq.com>
2019-12-22 17:38:42 +0800

    1

--- application/api/controller/ArticleManager.php
+++ application/api/controller/ArticleManager.php
@@ -30,32 +30,33 @@ class ArticleManager extends Api
 //        $data["rows"]=$model->with("user,articletype")->where(['article.status'=>"显示"])->limit($offset,$page_size)->select();
 //        $data["count"]=$model->where(['status'=>0])->count();
 
-        $query=(new Query())->table("fa_article")->alias("article");
-
+        $where[]=['article.status'=>"显示"];
 
         // 需要查找的类型.
         $articletype_id=$this->request->request("articletype_id",0);
         if($articletype_id){
-            $query=$query->where("articletype.articletype_id","=",$articletype_id);
+            $where[]=["articletype.articletype_id"=>$articletype_id];
         }
 
         // keyword 检索. 关键字检索.
 
         $keyword=$this->request->request("keyword","");
         if($keyword){
-            $query=$query->where("article.title|article.description|article.content","like","%".$keyword."%");
+            $where[]=["article.title|article.description|article.content"=>["like","%".$keyword."%"]];
         }
 
 
-
-        $data["rows"]=$query->field("article.*,articletype.name as articletype_name,user.username,user.avatar")
+        $query=new Query();
+        $data["rows"]=$query->table("fa_article")->alias("article")->field("article.*,articletype.name as articletype_name,user.username,user.avatar")
+            ->where($where)
             ->join("fa_articletype articletype","articletype.id=article.articletype_id","left")
             ->join("fa_user user","user.id=article.user_id","left")
             ->limit($offset,$page_size)->select();
 
 
 
-        $data["count"]=$query
+        $data["count"]=$query->table("fa_article")->alias("article")
+            ->where($where)
             ->join("fa_articletype articletype","articletype.id=article.articletype_id","left")
             ->join("fa_user user","user.id=article.user_id","left")
             ->limit($offset,$page_size)->count();
