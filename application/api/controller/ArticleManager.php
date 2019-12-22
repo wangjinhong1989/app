a9fff2fc0f045ab33e34676e41964aab0985dfd9
wangjinhong <392513170@qq.com>
2019-12-22 16:50:40 +0800

    1

--- application/api/controller/ArticleManager.php
+++ application/api/controller/ArticleManager.php
@@ -51,15 +51,24 @@ class ArticleManager extends Api
             ->join("fa_user as user","user.id=article.user_id")
             ->limit($offset,$page_size)->select();
 
+
+        $data["count"]=Db::table("fa_article as article")
+            ->where($where)
+            ->join("fa_articletype as articletype","articletype.id=article.articletype_id")
+            ->join("fa_user as user","user.id=article.user_id")
+            ->limit($offset,$page_size)->getLastSql();
+
+
+
         $data["page"]=$page;
 
-        foreach ($data["rows"] as $key=>$value){
-            $data["rows"][$key]["username"]=$data["rows"][$key]["user"]["username"];
-            $data["rows"][$key]["avatar"]=$data["rows"][$key]["user"]["avatar"];
-            $data["rows"][$key]["articletype_name"]=$data["rows"][$key]["articletype"]["name"];
-            unset($data["rows"][$key]["user"]);
-            unset($data["rows"][$key]["articletype"]);
-        }
+//        foreach ($data["rows"] as $key=>$value){
+//            $data["rows"][$key]["username"]=$data["rows"][$key]["user"]["username"];
+//            $data["rows"][$key]["avatar"]=$data["rows"][$key]["user"]["avatar"];
+//            $data["rows"][$key]["articletype_name"]=$data["rows"][$key]["articletype"]["name"];
+//            unset($data["rows"][$key]["user"]);
+//            unset($data["rows"][$key]["articletype"]);
+//        }
         $data["total_page"]=ceil($data["count"]/$page_size);
         $this->success("成功",$data);
     }
