88f1e87685ba3d496690a5538d2a775baeefc619
wangjinhong <392513170@qq.com>
2019-12-22 16:48:59 +0800

    1

--- application/api/controller/ArticleManager.php
+++ application/api/controller/ArticleManager.php
@@ -26,8 +26,30 @@ class ArticleManager extends Api
         $offset=($page-1)*$page_size;
         $model=new Article();
         $data=[];
-        $data["rows"]=$model->with("user,articletype")->where(['article.status'=>"显示"])->limit($offset,$page_size)->select();
-        $data["count"]=$model->where(['status'=>0])->count();
+//        $data["rows"]=$model->with("user,articletype")->where(['article.status'=>"显示"])->limit($offset,$page_size)->select();
+//        $data["count"]=$model->where(['status'=>0])->count();
+
+        $where=['article.status'=>"显示"];
+
+        // 需要查找的类型.
+        $articletype_id=$this->request->request("articletype_id",0);
+        if($articletype_id){
+            $where[]=["articletype.articletype_id","",$articletype_id];
+        }
+
+        // keyword 检索. 关键字检索.
+
+        $keyword=$this->request->request("keyword","");
+        if($keyword){
+            $where[]=["article.title|article.content|article.content","like","%".$keyword."%"];
+        }
+
+
+        $data["rows"]=Db::table("fa_article as article")
+            ->where($where)
+            ->join("fa_articletype as articletype","articletype.id=article.articletype_id")
+            ->join("fa_user as user","user.id=article.user_id")
+            ->limit($offset,$page_size)->select();
 
         $data["page"]=$page;
 
