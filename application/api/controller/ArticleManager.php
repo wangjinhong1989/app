149b2f99c27fec84d3e5c485f89b9dda61f79232
wangjinhong <392513170@qq.com>
2019-12-22 16:56:09 +0800

    1

--- application/api/controller/ArticleManager.php
+++ application/api/controller/ArticleManager.php
@@ -48,14 +48,14 @@ class ArticleManager extends Api
         $data["rows"]=Db::table("fa_article")->alias("article")
             ->where($where)
             ->join("fa_articletype  articletype","articletype.id=article.articletype_id","left")
-            ->join("fa_user  user","user.id=article.user_id")
+            ->join("fa_user  user","user.id=article.user_id","left")
             ->limit($offset,$page_size)->select();
 
 
         $data["count"]=Db::table("fa_article")->alias("article")
             ->where($where)
             ->join("fa_articletype  articletype","articletype.id=article.articletype_id","left")
-            ->join("fa_user  user","user.id=article.user_id")
+            ->join("fa_user  user","user.id=article.user_id","left")
             ->limit($offset,$page_size)->getLastSql();
 
 
