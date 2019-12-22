083dcc4c53d0b98a9dda3f91c5633892f3f58197
wangjinhong <392513170@qq.com>
2019-12-22 10:11:24 +0800

    1

--- application/api/controller/JiGuang.php
+++ application/api/controller/JiGuang.php
@@ -24,11 +24,38 @@ class JiGuang extends Api
      */
     public function send()
     {
+
+        $mobile = $this->request->request("mobile");
+        $event = $this->request->request("event");
+
+        if (!$mobile || !\think\Validate::regex($mobile, "^1\d{10}$")) {
+            $this->error(__('手机号不正确'));
+        }
+
+        if ($event) {
+            $userinfo = User::getByMobile($mobile);
+            if ($event == 'register' && $userinfo) {
+                //已被注册
+                $this->error(__('已被注册'));
+            } elseif (in_array($event, ['changemobile']) && $userinfo) {
+                //被占用
+                $this->error(__('已被占用'));
+            } elseif (in_array($event, ['changepwd', 'resetpwd']) && !$userinfo) {
+                //未注册
+                $this->error(__('未注册'));
+            }
+        }
         $mobile = $this->request->request("mobile");
 
         $client =  new \JiGuang\JSMS( Config::get("jiguang_app_key"),  Config::get("jiguang_master_secret"), [ 'disable_ssl' => true ]);
 
-        echo \GuzzleHttp\json_encode($client->sendCode($mobile, 1,12236));
+        $res=$client->sendCode($mobile, 1,12236);
+
+        if($res["http_code"]==200){
+            $this->success();
+        }else{
+            $this->error("短信错误",$res,1001);
+        }
     }
 
     /**
@@ -41,11 +68,39 @@ class JiGuang extends Api
     public function check()
     {
 
+        $mobile = $this->request->request("mobile");
+        $event = $this->request->request("event");
+        $event = $event ? $event : 'register';
+
+        if (!$mobile || !\think\Validate::regex($mobile, "^1\d{10}$")) {
+            $this->error(__('手机号不正确'));
+        }
+        if ($event) {
+            $userinfo = User::getByMobile($mobile);
+            if ($event == 'register' && $userinfo) {
+                //已被注册
+                $this->error(__('已被注册'));
+            } elseif (in_array($event, ['changemobile']) && $userinfo) {
+                //被占用
+                $this->error(__('已被占用'));
+            } elseif (in_array($event, ['changepwd', 'resetpwd']) && !$userinfo) {
+                //未注册
+                $this->error(__('未注册'));
+            }
+        }
+
+
         $code = $this->request->request("code");
+        $msg_id = $this->request->request("msg_id");
 
         $client =  new \JiGuang\JSMS(config("jiguang_app_key"), config("jiguang_master_secret"), [ 'disable_ssl' => true ]);
 
-        $this->success("",$client->checkCode(1, $code));
+        $res=$this->success("",$client->checkCode($msg_id, $code));
+        if($res["http_code"]==200){
+            $this->success();
+        }else{
+            $this->error("短信错误",$res,1001);
+        }
 
     }
 }
