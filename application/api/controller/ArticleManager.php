f314d46bfa0ea5c02f0caf8a398c13051297d2a4
wangjinhong <392513170@qq.com>
2019-12-22 16:31:10 +0800

    1

--- application/api/controller/ArticleManager.php
+++ application/api/controller/ArticleManager.php
@@ -4,6 +4,9 @@ namespace app\api\controller;
 
 use app\admin\model\Article;
 use app\common\controller\Api;
+use think\Db;
+use think\view\driver\Think;
+
 /**
  * 首页接口
  */
@@ -25,7 +28,16 @@ class ArticleManager extends Api
         $data=[];
         $data["rows"]=$model->with("user,articletype")->where(['article.status'=>"显示"])->limit($offset,$page_size)->select();
         $data["count"]=$model->where(['status'=>0])->count();
+
         $data["page"]=$page;
+
+        foreach ($data["rows"] as $key=>$value){
+            $data["rows"][$key]["username"]=$data["rows"][$key]["user"]["username"];
+            $data["rows"][$key]["avatar"]=$data["rows"][$key]["user"]["avatar"];
+            $data["rows"][$key]["articletype_name"]=$data["rows"][$key]["articletype"]["name"];
+            unset($data["rows"][$key]["user"]);
+            unset($data["rows"][$key]["articletype"]);
+        }
         $data["total_page"]=ceil($data["count"]/$page_size);
         $this->success("成功",$data);
     }
