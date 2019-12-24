6f7e1fa1196383ae73dd2b05fc00b88d01cc0ba3
wangjinhong <392513170@qq.com>
2019-12-24 21:53:16 +0800

    1

---
+++ application/api/controller/Banner.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace app\api\controller;
+
+use app\common\controller\Api;
+use app\admin\model\Guanggao;
+use think\Cache;
+use think\Config;
+
+/**
+ * 首页接口
+ */
+class Banner extends Api
+{
+    protected $noNeedLogin = ['*'];
+    protected $noNeedRight = ['*'];
+
+    /**
+     * 首页
+     *
+     */
+    public function Lists()
+    {
+        $lists=(new \app\admin\model\Banner())->where(['status'=>'显示'])->select();
+
+        $this->success("成功",$lists);
+    }
+}
