8d016d9891154c2515a2c86caf6d0a46c43efcfe
wangjinhong <392513170@qq.com>
2019-12-25 23:24:48 +0800

    1

---
+++ application/admin/lang/zh-cn/recomment.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'Id'            => '编号',
+    'Img'           => 'banner图',
+    'Url'           => '链接地址',
+    'Article_id'    => '文章',
+    'Url_type'      => '内外链',
+    'Status'        => '状态',
+    'Article.title' => '标题'
+];
