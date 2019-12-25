2b4738fc183d3f959f2c67ba8ce77cc0fa81409c
wangjinhong <392513170@qq.com>
2019-12-25 21:38:08 +0800

    1

---
+++ application/admin/lang/zh-cn/read_history.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'Id'            => '编号',
+    'User_id'       => '用户',
+    'Article_id'    => '文章',
+    'Time'          => '创建时间',
+    'Article.title' => '标题',
+    'User.username' => '用户名'
+];
