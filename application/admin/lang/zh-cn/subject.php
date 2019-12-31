ae5e65a5676b3b7399894fa938b813aea659dfd2
wangjinhong <392513170@qq.com>
2019-12-31 20:37:44 +0800

    1

---
+++ application/admin/lang/zh-cn/subject.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'Id'          => '专题ID',
+    'Title'       => '标题',
+    'Description' => '描述',
+    'Image'       => '封面',
+    'Come_from'   => '来源',
+    'Url'         => '来源地址',
+    'Status'      => '状态',
+    'Sort'        => '排序',
+    'Read_count'  => '阅读次数',
+    'Content'     => '内容',
+    'Create_time' => '创建时间'
+];
---
+++ public/assets/js/backend/subject.js
@@ -0,0 +1,58 @@
+define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefined, Backend, Table, Form) {
+
+    var Controller = {
+        index: function () {
+            // 初始化表格参数配置
+            Table.api.init({
+                extend: {
+                    index_url: 'subject/index' + location.search,
+                    add_url: 'subject/add',
+                    edit_url: 'subject/edit',
+                    del_url: 'subject/del',
+                    multi_url: 'subject/multi',
+                    table: 'subject',
+                }
+            });
+
+            var table = $("#table");
+
+            // 初始化表格
+            table.bootstrapTable({
+                url: $.fn.bootstrapTable.defaults.extend.index_url,
+                pk: 'id',
+                sortName: 'id',
+                columns: [
+                    [
+                        {checkbox: true},
+                        {field: 'id', title: __('Id')},
+                        {field: 'title', title: __('Title')},
+                        {field: 'description', title: __('Description')},
+                        {field: 'image', title: __('Image'), events: Table.api.events.image, formatter: Table.api.formatter.image},
+                        {field: 'come_from', title: __('Come_from')},
+                        {field: 'url', title: __('Url'), formatter: Table.api.formatter.url},
+                        {field: 'status', title: __('Status'), searchList: {"显示":__('显示'),"隐藏":__('隐藏')}, formatter: Table.api.formatter.status},
+                        {field: 'sort', title: __('Sort')},
+                        {field: 'read_count', title: __('Read_count')},
+                        {field: 'create_time', title: __('Create_time'), operate:'RANGE', addclass:'datetimerange', formatter: Table.api.formatter.datetime},
+                        {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
+                    ]
+                ]
+            });
+
+            // 为表格绑定事件
+            Table.api.bindevent(table);
+        },
+        add: function () {
+            Controller.api.bindevent();
+        },
+        edit: function () {
+            Controller.api.bindevent();
+        },
+        api: {
+            bindevent: function () {
+                Form.api.bindevent($("form[role=form]"));
+            }
+        }
+    };
+    return Controller;
+});
