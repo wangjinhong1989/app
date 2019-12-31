fe342b9c725d5485dd58fe270fae3b303ec6cd4b
wangjinhong <392513170@qq.com>
2019-12-31 22:40:39 +0800

    1

--- application/api/controller/ArticleManager.php
+++ application/api/controller/ArticleManager.php
@@ -34,6 +34,7 @@ class ArticleManager extends Api
         $data=[];
         $where=[];
         $where["article.status"]=["eq","显示"];
+        $where["articletype.status"]=["eq","显示"];
 
         // 需要查找的类型. 可以设置多个.
         $articletype_id=$this->request->request("articletype_id","");
