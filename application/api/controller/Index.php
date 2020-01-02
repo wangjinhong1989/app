c4e72563c2a68cbed79278e38c1b8f8a01bb439f
王金宏 <2709989114@qq.com>
2020-01-02 16:23:47 +0800

    1

--- application/api/controller/Index.php
+++ application/api/controller/Index.php
@@ -3,6 +3,7 @@
 namespace app\api\controller;
 
 use app\common\controller\Api;
+use yuntongxun\REST;
 
 /**
  * 首页接口
@@ -19,5 +20,56 @@ class Index extends Api
     public function index()
     {
         $this->success('请求成功');
+    }
+
+    public function test()
+    {
+        $this->sendTemplateSMS("17380613281",['1234','5678'],1);
+
+    }
+
+    function sendTemplateSMS($to, $datas, $tempId)
+    {
+        //主帐号
+        $accountSid = '8aaf07086f17620f016f319cf6d61120';
+
+        //主帐号Token
+        $accountToken = '32ea281adbb94320ad758a2b1c061ecb';
+
+        //应用Id
+        $appId = '8aaf07086f17620f016f319cf73d1126';
+
+        //请求地址，格式如下，不需要写https://
+        $serverIP = 'app.cloopen.com';
+
+        //请求端口
+        $serverPort = '8883';
+
+        //REST版本号
+        $softVersion = '2013-12-26';
+        $rest = new REST($serverIP, $serverPort, $softVersion);
+        $rest->setAccount($accountSid, $accountToken);
+        $rest->setAppId($appId);
+
+        // 发送模板短信
+        echo "Sending TemplateSMS to $to <br/>";
+        $result = $rest->sendTemplateSMS($to, $datas, $tempId);
+        if ($result == NULL) {
+            echo "result error!";
+            break;
+        }
+        if ($result->statusCode != 0) {
+            echo "error code :" . $result->statusCode . "<br>";
+            echo "error msg :" . $result->statusMsg . "<br>";
+            //TODO 添加错误处理逻辑
+        } else {
+            echo "Sendind TemplateSMS success!<br/>";
+            // 获取返回信息
+            $smsmessage = $result->TemplateSMS;
+            echo "dateCreated:" . $smsmessage->dateCreated . "<br/>";
+            echo "smsMessageSid:" . $smsmessage->smsMessageSid . "<br/>";
+            //TODO 添加成功处理逻辑
+        }
+
     }
 }
