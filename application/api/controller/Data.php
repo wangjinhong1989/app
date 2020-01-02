bec53628045490ec72124ef2daeebb79baf3b717
王金宏 <2709989114@qq.com>
2020-01-02 14:11:26 +0800

    1

--- application/api/controller/Data.php
+++ application/api/controller/Data.php
@@ -2,6 +2,8 @@
 
 namespace app\api\controller;
 
+use app\admin\model\Article;
+use app\admin\model\Articletype;
 use app\common\controller\Api;
 use app\admin\model\Guanggao;
 use think\Cache;
@@ -22,5 +24,28 @@ class Data extends Api
             $mobile="13800000".$i;
             $ret = $this->auth->register("username_".$i, "123456", '', $mobile, []);
         }
+    }
+
+    // 根据分类添加各类文章.
+    public function article(){
+            $type=new Articletype();
+            $types=$type->where([])->select()->toArray();
+            $user=(new \app\admin\model\User())->where([])->select()->toArray();
+            $labels=(new \app\admin\model\Label())->where([])->select()->toArray();
+
+            $article= new Article();
+
+            $time=time();
+            $article->create(["title"=>"title".$time,
+                "description"=>" description".$time,
+                "img"=>"http://app.luxiaogui.cn//uploads/20191230/8f4c7653f9b9333617a2664960298ddf.jpg",
+                "content"=>" content".$time,
+                "is_mine"=>"是",
+                "is_reply"=>"是",
+                'articletype_id'=>$types[rand(0,count($types)-1)]['id'],
+                'label_ids'=>$labels[rand(0,count($labels)-1)]['id'].",".$labels[rand(0,count($labels)-1)]['id'],
+                'user_id'=>$user[rand(0,count($user)-1)]['id']
+            ]);
+
     }
 }
