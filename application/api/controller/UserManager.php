a5fed96dcf157827216f5ac1d3c96ffeba674490
王金宏 <2709989114@qq.com>
2020-01-07 17:10:26 +0800

    1

--- application/api/controller/UserManager.php
+++ application/api/controller/UserManager.php
@@ -44,7 +44,7 @@ class UserManager extends Api
         //  非空.
         $follow_id=$this->request->request("follow_id","");
         if($follow_id=="已关注"){
-            $where["guanzhu.follow_id"]=["EXP","IS NOT NULL"];
+            $where["guanzhu.follow_id"]=["gt",0];
 
         }else if($follow_id=="未关注"){
             $where["guanzhu.follow_id"]=["EXP","IS NULL"];
