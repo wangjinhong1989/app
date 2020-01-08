0b0751e08ae8cd697f63a30974921bef013db16a
王金宏 <2709989114@qq.com>
2020-01-08 09:42:20 +0800

    1

--- application/api/controller/ArticleManager.php
+++ application/api/controller/ArticleManager.php
@@ -110,7 +110,7 @@ class ArticleManager extends Api
         // 请求的标签.
 
         $query=new Query();
-        $data["rows"]=$query->table("fa_article")->alias("article")->field("article.*,articletype.name as articletype_name,user.username,user.avatar,kong_hao.count_lihao,konghao.count_likong")
+        $data["rows"]=$query->table("fa_article")->alias("article")->field("article.*,articletype.name as articletype_name,user.username,user.avatar,kong_hao.count_lihao,kong_hao.count_likong")
             ->where($where)
             ->whereExp('',$whereExp)
             ->join("fa_articletype articletype","articletype.id=article.articletype_id","left")
