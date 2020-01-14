18a304ac362939f185ac4991146450bedb2edd2d
王金宏 <2709989114@qq.com>
2020-01-14 09:46:57 +0800

    1

--- addons/third/library/Wechat.php
+++ addons/third/library/Wechat.php
@@ -64,9 +64,12 @@ class Wechat
     public function getUserInfo($params = [])
     {
         $params = $params ? $params : $_GET;
-        if (isset($params['access_token']) || (isset($params['state']) && $params['state'] == Session::get('state') && isset($params['code']))) {
+//        if (isset($params['access_token']) || (isset($params['state']) && $params['state'] == Session::get('state') && isset($params['code']))) {
+        if (isset($params['access_token']) || (isset($params['code']))) {
             //获取access_token
             $data = isset($params['code']) ? $this->getAccessToken($params['code']) : $params;
+
+            dd($data);
             $access_token = isset($data['access_token']) ? $data['access_token'] : '';
             $refresh_token = isset($data['refresh_token']) ? $data['refresh_token'] : '';
             $expires_in = isset($data['expires_in']) ? $data['expires_in'] : 0;
