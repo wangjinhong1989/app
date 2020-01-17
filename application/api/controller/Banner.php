8e767adb103e7ef458e8752ed18e72587acc1323
王金宏 <2709989114@qq.com>
2020-01-17 13:22:15 +0800

    1

--- application/api/controller/Banner.php
+++ application/api/controller/Banner.php
@@ -40,7 +40,7 @@ class Banner extends Api
 
 
         foreach ($lists as &$l){
-            $l["url"]="http://app.biyouliao8.com".$l["url"];
+            $l["img"]="http://app.biyouliao8.com".$l["img"];
         }
         $data["page"]=$page;
         $data["rows"]=$lists;
