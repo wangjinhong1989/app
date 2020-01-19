56d3e543792da09ffff9d03135cf3c537a58560a
王金宏 <2709989114@qq.com>
2020-01-19 11:21:01 +0800

    1

---
+++ application/api/controller/Discover.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace app\api\controller;
+
+
+use app\admin\model\HotSearch;
+use app\common\controller\Api;
+/**
+ * 首页接口
+ */
+class Discover extends Api
+{
+    protected $noNeedLogin = [];
+    protected $noNeedRight = ['*'];
+
+    /**
+     * 热搜
+     *
+     */
+    public function Lists()
+    {
+
+        $page=$this->request->request("page",1);
+        $page_size=$this->request->request("page_size",5);
+        $order=$this->request->request("order","weigh");
+        $sort=$this->request->request("sort","asc");
+        $offset=($page-1)*$page_size;
+
+        $data=[];
+        $lists=( new \app\admin\model\Discover())->where(['status'=>"显示"])->limit($offset,$page_size)->order($order,$sort)->select();
+        $count=( new \app\admin\model\Discover())->where(['status'=>"显示"])->count();
+
+
+        $data["page"]=$page;
+        $data["rows"]=$lists;
+        $data["count"]=$count;
+
+        $data["total_page"]=ceil($data["count"]/$page_size);
+        $this->success("成功",$data);
+
+    }
+}
