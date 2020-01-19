7865f661ce3765a3c0b198052bb7ebbc00dcf6fe
wjh <392513170@qq.com>
2020-01-19 11:11:13 +0800

    1

---
+++ application/admin/lang/zh-cn/discover.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'Title'       => '标题',
+    'Description' => '描述',
+    'Weigh'       => '权重',
+    'Status'      => '有效无效',
+    'Url'         => '原文链接',
+    'Image'       => '封面',
+    'Read_count'  => '阅读次数',
+    'Show_count'  => '展示次数',
+    'Content'     => '内容'
+];
