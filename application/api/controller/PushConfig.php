ef53c449f7ffb39136d33df60d79b1f3d3a94a01
wangjinhong <392513170@qq.com>
2020-01-21 18:17:08 +0800

    1

--- application/api/controller/PushConfig.php
+++ application/api/controller/PushConfig.php
@@ -45,27 +45,59 @@ class PushConfig extends Api
             $model= new \app\admin\model\PushConfig();
             $user = $this->auth->getUser();
             $user_id = $user->id;
-            $id = $this->request->request('id',"0");
-            $is_accept_notify = $this->request->request('is_accept_notify',"是");
-            $is_article_notify = $this->request->request('is_article_notify',"是");
-            $is_kuaixun_notify = $this->request->request('is_kuaixun_notify',"是");
-            $is_follow_notify= $this->request->request('is_follow_notify',"是");
+            //$id = $this->request->request('id',"0");
+//            $is_accept_notify = $this->request->request('is_accept_notify',"是");
+//            $is_article_notify = $this->request->request('is_article_notify',"是");
+//            $is_kuaixun_notify = $this->request->request('is_kuaixun_notify',"是");
+//            $is_follow_notify= $this->request->request('is_follow_notify',"是");
+            $param= $this->request->request('param',"");
 
-            $info=$model->where(["id"=>$id,"user_id"=>$user_id])->find();
 
-            if(!$info||!$id||!$is_accept_notify||!$is_article_notify||!$is_kuaixun_notify||!$is_follow_notify){
+            $info=$model->where(["user_id"=>$user_id])->find();
+
+            if(!$param){
                 return $this->error(__('参数存在空'));
                 die;
             }
 
-            $arr=["是","否"];
-            if(!in_array($is_accept_notify,$arr)||!in_array($is_article_notify,$arr)||!in_array($is_kuaixun_notify,$arr)||!in_array($is_follow_notify,$arr)){
-                return $this->error(__('参数存在错误'));
+            if(!$info){
+                $model->create(
+                    [
+                        "is_accept_notify"=>"是",
+                        "is_article_notify"=>"是",
+                        "is_kuaixun_notify"=>"是",
+                        "is_follow_notify"=>"是",
+                        "need_voice"=>"是",
+                        "status"=>"是",
+                        "create_time"=>time(),
+
+                    ]
+                );
             }
-            $info->is_accept_notify=$is_accept_notify;
-            $info->is_article_notify=$is_article_notify;
-            $info->is_kuaixun_notify=$is_kuaixun_notify;
-            $info->is_follow_notify=$is_follow_notify;
+            $info=$model->where(["user_id"=>$user_id])->find();
+            if($param=="is_accept_notify"){
+                if($info->is_accept_notify=="是"){
+                    $info->is_accept_notify="否";
+                }
+            }
+            if($param=="is_article_notify"){
+                if($info->is_article_notify=="是"){
+                    $info->is_article_notify="否";
+                }
+            }
+
+            if($param=="is_kuaixun_notify"){
+                if($info->is_kuaixun_notify=="是"){
+                    $info->is_kuaixun_notify="否";
+                }
+            }
+
+            if($param=="is_follow_notify"){
+                if($info->is_follow_notify=="是"){
+                    $info->is_follow_notify="否";
+                }
+            }
+
             $info->save();
             return $this->success();
         } catch (Exception $e) {
