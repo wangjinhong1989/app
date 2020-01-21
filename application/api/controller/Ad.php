dcd652dacbd25c40ae54c935378d5b8936ee014c
wangjinhong <392513170@qq.com>
2020-01-21 11:22:25 +0800

    1

--- application/api/controller/Ad.php
+++ application/api/controller/Ad.php
@@ -24,7 +24,7 @@ class Ad extends Api
     {
         $model=    new Query();
 
-        $lists=$model->table("fa_guanggao")->where(['status'=>'显示'])->orderRaw("rand()")->select();
+        $lists=$model->table("fa_guanggao")->where(['status'=>'显示'])->orderRaw("id",["rand()"])->select();
         dd($model->getLastSql());
         //phpinfo();
         Cache::store('redis')->clear();
