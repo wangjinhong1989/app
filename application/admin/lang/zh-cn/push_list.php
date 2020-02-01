969b67abf749cf2949b3160b679d18bca24a95fa
wjh <392513170@qq.com>
2020-02-01 15:49:02 +0800

    1

---
+++ application/admin/lang/zh-cn/push_list.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'Id'            => '编号',
+    'User_id'       => '用户ID',
+    'Push_type_id'  => '推送类型ID',
+    'Content'       => '推送内容',
+    'Create_time'   => '时间',
+    'User.username' => '用户名',
+    'Pushtype.type' => '推送类型'
+];
