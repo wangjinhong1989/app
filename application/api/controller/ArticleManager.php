8006afa9d3f185e3509a54ccdc024e267c4f0c20
wangjinhong <392513170@qq.com>
2020-02-06 17:26:42 +0800

    1

--- application/api/controller/ArticleManager.php
+++ application/api/controller/ArticleManager.php
@@ -585,7 +585,23 @@ class ArticleManager extends Api
                 return $this->error(__('标题或者内容为空'));
             }
 
-            //$data["content"]=($data["content"]);
+            $data["content"]=str_replace('<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-16">
+<meta http-equiv="Content-Style-Type" content="text/css">
+<title></title>
+<meta name="Generator" content="Cocoa HTML Writer">
+<style type="text/css">
+p.p1 {margin: 0.0px 0.0px 0.0px 0.0px; font: 16.0px \'.AppleSystemUIFont\'}
+span.s1 {font-family: \'.SFUI-Regular\'; font-weight: normal; font-style: normal; font-size: 16.00px}
+span.s2 {font-family: \'Helvetica\'; font-weight: normal; font-style: normal; font-size: 12.00px}
+</style>
+</head>
+<body>',"",$data["content"]);
+
+            $data["content"]=str_replace('</body>
+</html>',"",$data["content"]);
             // 这里要求传描述
             if($data["articletype_id"]==2){
                 if(!$data["description"]){
