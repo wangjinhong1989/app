afbec72817cd88451a80dcb5c5c3426ffe669e59
wangjinhong <392513170@qq.com>
2020-02-06 21:38:14 +0800

    1

--- application/api/controller/ArticleManager.php
+++ application/api/controller/ArticleManager.php
@@ -484,6 +484,23 @@ class ArticleManager extends Api
 //        file_put_contents("1",$detail->getLastSql());
         if($detail){
 
+            $detail["content"]=str_replace('<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-16">
+<meta http-equiv="Content-Style-Type" content="text/css">
+<title></title>
+<meta name="Generator" content="Cocoa HTML Writer">
+<style type="text/css">
+p.p1 {margin: 0.0px 0.0px 0.0px 0.0px; font: 16.0px \'.AppleSystemUIFont\'}
+span.s1 {font-family: \'.SFUI-Regular\'; font-weight: normal; font-style: normal; font-size: 16.00px}
+span.s2 {font-family: \'Helvetica\'; font-weight: normal; font-style: normal; font-size: 12.00px}
+</style>
+</head>
+<body>',"",$detail["content"]);
+            $detail["content"]=str_replace('</body></html>',"",$detail["content"]);
+
+
             $detail["content"]="<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\">
 <html>
 <head>
