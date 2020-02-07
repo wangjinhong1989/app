cb23322104f377ed95c4d18f05ef21e6e7c2b474
wangjinhong <392513170@qq.com>
2020-02-07 15:29:47 +0800

    1

--- application/admin/command/Top.php
+++ application/admin/command/Top.php
@@ -38,7 +38,7 @@ class Top extends Command
 
         $query = new Query();
 
-        $query->table("fa_article")->where(["begin_time"=>["lt",time()],"top"=>["eq","置顶"]])->whereOr(["end_time"=>["gt",time()],"top"=>["eq","置顶"]])->whereOr(["weigh"=>["eq",0]])->chunk(100, function ($list) {
+        $query->table("fa_article")->where(["begin_time"=>["lt",time()],"top"=>["eq","置顶"]])->whereOr(["end_time"=>["gt",time()],"top"=>["eq","置顶"]])->chunk(100, function ($list) {
 
             // 需要推送的列表.
             foreach ($list as  $l){
@@ -49,6 +49,25 @@ class Top extends Command
                 $model->save();
             }
         });
+
+        $query->table("fa_article")->where(["weigh"=>["eq",0]])->chunk(100, function ($list) {
+
+            // 需要推送的列表.
+            foreach ($list as  $l){
+
+                $model=(new Article())->where(["id"=>$l["id"]])->find();
+
+                if($model->top=="置顶"){
+                    $model->weigh=time();
+                }else{
+                    $model->weigh=$l["id"];
+                }
+
+                $model->save();
+            }
+        });
+
+        //->whereOr(["weigh"=>["eq",0]]
     }
 
 
