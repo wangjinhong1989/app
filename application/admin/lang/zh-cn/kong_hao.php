7226b78ccfe929e4c52251479bc970abd7d22e11
wjh <392513170@qq.com>
2020-02-07 17:46:59 +0800

    1

---
+++ application/admin/lang/zh-cn/kong_hao.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'Id'           => 'id',
+    'Count_likong' => '利空',
+    'Count_lihao'  => '利好',
+    'Article_id'   => '文章ID'
+];
---
+++ public/assets/js/backend/kong_hao.js
@@ -0,0 +1,52 @@
+define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefined, Backend, Table, Form) {
+
+    var Controller = {
+        index: function () {
+            // 初始化表格参数配置
+            Table.api.init({
+                extend: {
+                    index_url: 'kong_hao/index' + location.search,
+                    add_url: 'kong_hao/add',
+                    edit_url: 'kong_hao/edit',
+                    del_url: 'kong_hao/del',
+                    multi_url: 'kong_hao/multi',
+                    table: 'kong_hao',
+                }
+            });
+
+            var table = $("#table");
+
+            // 初始化表格
+            table.bootstrapTable({
+                url: $.fn.bootstrapTable.defaults.extend.index_url,
+                pk: 'id',
+                sortName: 'id',
+                columns: [
+                    [
+                        {checkbox: true},
+                        {field: 'id', title: __('Id')},
+                        {field: 'count_likong', title: __('Count_likong')},
+                        {field: 'count_lihao', title: __('Count_lihao')},
+                        {field: 'article_id', title: __('Article_id')},
+                        {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
+                    ]
+                ]
+            });
+
+            // 为表格绑定事件
+            Table.api.bindevent(table);
+        },
+        add: function () {
+            Controller.api.bindevent();
+        },
+        edit: function () {
+            Controller.api.bindevent();
+        },
+        api: {
+            bindevent: function () {
+                Form.api.bindevent($("form[role=form]"));
+            }
+        }
+    };
+    return Controller;
+});
