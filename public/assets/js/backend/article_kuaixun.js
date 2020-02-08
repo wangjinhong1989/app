631df1f97f4d1ec28b1ecac19254b18a5b848e17
wangjinhong <392513170@qq.com>
2020-02-08 11:13:43 +0800

    1

---
+++ public/assets/js/backend/article_kuaixun.js
@@ -0,0 +1,61 @@
+define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefined, Backend, Table, Form) {
+
+    var Controller = {
+        index: function () {
+            // 初始化表格参数配置
+            Table.api.init({
+                extend: {
+                    index_url: 'article/index' + location.search,
+                    add_url: 'article/add',
+                    edit_url: 'article/edit',
+                    del_url: 'article/del',
+                    multi_url: 'article/multi',
+                    table: 'article',
+                }
+            });
+
+            var table = $("#table");
+
+            // 初始化表格
+            table.bootstrapTable({
+                url: $.fn.bootstrapTable.defaults.extend.index_url,
+                pk: 'id',
+                sortName: 'id',
+                columns: [
+                    [
+                        {checkbox: true},
+                        {field: 'id', title: __('Id')},
+                        {field: 'title', title: __('Title')},
+                        {field: 'description', title: __('Description')},
+                        {field: 'status', title: __('Status'), searchList: {"显示":__('显示'),"隐藏":__('隐藏')}, formatter: Table.api.formatter.status},
+                        {field: 'come_from', title: __('Come_from')},
+                        {field: 'label_ids', title: __('Label_ids')},
+                        {field: 'url', title: __('Url'), formatter: Table.api.formatter.url},
+                        {field: 'img', title: __('Img')},
+                        {field: 'read_count', title: __('Read_count')},
+                        {field: 'show_count', title: __('Show_count')},
+                        {field: 'articletype.name', title: __('Articletype.name')},
+                        {field: 'label.name', title: __('Label.name')},
+                        {field: 'user.username', title: __('User.username')},
+                        {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
+                    ]
+                ]
+            });
+
+            // 为表格绑定事件
+            Table.api.bindevent(table);
+        },
+        add: function () {
+            Controller.api.bindevent();
+        },
+        edit: function () {
+            Controller.api.bindevent();
+        },
+        api: {
+            bindevent: function () {
+                Form.api.bindevent($("form[role=form]"));
+            }
+        }
+    };
+    return Controller;
+});
