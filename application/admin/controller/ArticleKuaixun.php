b98da06c848a72a09d5fe54cf9e556be93408074
wangjinhong <392513170@qq.com>
2020-02-09 15:17:55 +0800

    1

--- application/admin/controller/ArticleKuaixun.php
+++ application/admin/controller/ArticleKuaixun.php
@@ -86,4 +86,149 @@ class ArticleKuaixun extends Backend
     }
 
 
+    /**
+     * 添加
+     */
+    public function add()
+    {
+        if ($this->request->isPost()) {
+            $params = $this->request->post("row/a");
+            if ($params) {
+
+                if($params["top"]=="置顶"){
+                    if(empty($params["begin_time"])||empty($params["end_time"])){
+                        $this->error("请填写置顶时间");
+                    }
+                    if(strtotime($params["begin_time"])>=strtotime($params["end_time"])){
+                        $this->error("置顶时间开始时间大于等于结束时间");
+                    }
+                }
+
+                $params = $this->preExcludeFields($params);
+
+                if ($this->dataLimit && $this->dataLimitFieldAutoFill) {
+                    $params[$this->dataLimitField] = $this->auth->id;
+                }
+                $result = false;
+                Db::startTrans();
+                try {
+                    //是否采用模型验证
+                    if ($this->modelValidate) {
+                        $name = str_replace("\\model\\", "\\validate\\", get_class($this->model));
+                        $validate = is_bool($this->modelValidate) ? ($this->modelSceneValidate ? $name . '.add' : $name) : $this->modelValidate;
+                        $this->model->validateFailException(true)->validate($validate);
+                    }
+
+                    $result = $this->model->allowField(true)->save($params);
+
+                    if($params["top"]=="置顶"){
+                        if(strtotime($params["begin_time"])<=time()&&strtotime($params["end_time"])>=time())
+                            $params["weigh"]=time();
+                        else
+                            $params["weigh"]=$this->model->getLastInsID();
+                    }else {
+                        $params["weigh"]=$this->model->getLastInsID();
+                    }
+                    $params["id"]=$this->model->getLastInsID();
+                    $result = $this->model->allowField(true)->save($params);
+                    Db::commit();
+                } catch (ValidateException $e) {
+                    Db::rollback();
+                    $this->error($e->getMessage());
+                } catch (PDOException $e) {
+                    Db::rollback();
+                    $this->error($e->getMessage());
+                } catch (Exception $e) {
+                    Db::rollback();
+                    $this->error($e->getMessage());
+                }
+                if ($result !== false) {
+                    $this->success();
+                } else {
+                    $this->error(__('No rows were inserted'));
+                }
+            }
+            $this->error(__('Parameter %s can not be empty', ''));
+        }
+        return $this->view->fetch();
+    }
+
+    /**
+     * 编辑
+     */
+    public function edit($ids = null)
+    {
+        $row = $this->model->get($ids);
+        if (!$row) {
+            $this->error(__('No Results were found'));
+        }
+
+
+        $adminIds = $this->getDataLimitAdminIds();
+        if (is_array($adminIds)) {
+            if (!in_array($row[$this->dataLimitField], $adminIds)) {
+                $this->error(__('You have no permission'));
+            }
+        }
+        if ($this->request->isPost()) {
+            $params = $this->request->post("row/a");
+            if ($params) {
+
+                if($params["top"]=="置顶"){
+                    if(empty($params["begin_time"])||empty($params["end_time"])){
+                        $this->error("请填写置顶时间");
+                    }
+                    if(strtotime($params["begin_time"])>=strtotime($params["end_time"])){
+                        $this->error("置顶时间开始时间大于等于结束时间");
+                    }
+                }
+
+                $params = $this->preExcludeFields($params);
+                $result = false;
+                Db::startTrans();
+                try {
+                    //是否采用模型验证
+                    if ($this->modelValidate) {
+                        $name = str_replace("\\model\\", "\\validate\\", get_class($this->model));
+                        $validate = is_bool($this->modelValidate) ? ($this->modelSceneValidate ? $name . '.edit' : $name) : $this->modelValidate;
+                        $row->validateFailException(true)->validate($validate);
+                    }
+
+                    if($params["top"]=="置顶"){
+                        if(strtotime($params["begin_time"])<=time()&&strtotime($params["end_time"])>=time())
+                            $params["weigh"]=time();
+                        else
+                            $params["weigh"]=$ids;
+
+                    }else {
+                        $params["weigh"]=$ids;
+                    }
+
+                    $result = $row->allowField(true)->save($params);
+
+                    Db::commit();
+                    dd($result);
+                } catch (ValidateException $e) {
+                    Db::rollback();
+                    $this->error($e->getMessage());
+                } catch (PDOException $e) {
+                    Db::rollback();
+                    $this->error($e->getMessage());
+                } catch (Exception $e) {
+                    Db::rollback();
+                    $this->error($e->getMessage());
+                }
+                if ($result !== false) {
+                    $this->success();
+                } else {
+                    $this->error(__('No rows were updated'));
+                }
+            }
+            $this->error(__('Parameter %s can not be empty', ''));
+        }
+        $this->view->assign("row", $row);
+        return $this->view->fetch();
+    }
+
+
 }
