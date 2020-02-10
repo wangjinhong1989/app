0a44dac1cef66a952dd993c1fb2e3dfc4e251867
wangjinhong <392513170@qq.com>
2020-02-10 17:11:16 +0800

    1

--- application/api/controller/TypeArticle.php
+++ application/api/controller/TypeArticle.php
@@ -40,7 +40,7 @@ class TypeArticle extends Api
     public function neq_kuaixun_guanzhu()
     {
         $where=['status'=>"显示"];
-        $lists=( new Articletype())->where($where)->order("weigh","desc")->select();
+        $lists=( new Articletype())->where($where)->order("weigh","desc")->select()->toArray();
         foreach ($lists as $key=>$list){
             if($list["id"]==2||$list["id"]==7){
                 unset($lists[$key]);
