7fd7506aa1d3cfe3986276c812cf096891316c73
wangjinhong <392513170@qq.com>
2020-02-11 19:16:34 +0800

    1

--- application/api/controller/Label.php
+++ application/api/controller/Label.php
@@ -33,10 +33,10 @@ class Label extends Api
 
         $query=new Query();
         $lists=$query->table("fa_label")->where(["status"=>"显示"])->whereNotIn("id",function ($query){
-           return $query->table("fa_mylabel")->where("user_id",$this->auth->id)->filed("id")->select();
+           return $query->table("fa_mylabel")->where("user_id",$this->auth->id)->field("id")->select();
         })->limit($offset,$page_size)->select();
         $count=$query->table("fa_label")->where(["status"=>"显示"])->whereNotIn("id",function ($query){
-            return $query->table("fa_mylabel")->where("user_id",$this->auth->id)->filed("id")->select();
+            return $query->table("fa_mylabel")->where("user_id",$this->auth->id)->field("id")->select();
         })->count();
 
         $data["page"]=$page;
