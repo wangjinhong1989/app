c1a31c5068c9975f0f84cdfb3c0b70d4cdd54f5c
wangjinhong <392513170@qq.com>
2020-02-12 11:17:10 +0800

    1

--- application/api/controller/ProblemPlatform.php
+++ application/api/controller/ProblemPlatform.php
@@ -27,18 +27,14 @@ class ProblemPlatform extends Api
 
         $data=[];
 
-        $data["rows"]=(new Problem())->where(['status'=>'显示'])->limit($offset,$page_size)->order("id","desc")->select()->toArray();
+        $data["rows"]=(new Problem())->where(['status'=>'显示'])->limit($offset,$page_size)->order("id","desc")->select();
         $data["count"]=(new Problem())->where(['status'=>'显示'])->count();
 
         $data["page"]=$page;
 
         foreach ($data["rows"] as $k=>&$v){
-            $v["time"]= $t=date("Y-m-d H:i:s",$v["time"]);
-            $data["rows"][$k]["time"]=$t;
-            dd($k);
-            dd($t);
-            dd($v);
-            dd($data["rows"][$k]["time"]);
+            $v->time=date("Y-m-d H:i:s",$v->time);
+
         }
         $data["total_page"]=ceil($data["count"]/$page_size);
         $this->success("成功",$data);
