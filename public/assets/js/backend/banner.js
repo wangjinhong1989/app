0c94ce69ec97a1bc9ad68aaa66a8e35cb1680259
wangjinhong <392513170@qq.com>
2020-02-14 14:54:00 +0800

    1

--- public/assets/js/backend/banner.js
+++ public/assets/js/backend/banner.js
@@ -25,7 +25,6 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                     [
                         {checkbox: true},
                         {field: 'id', title: __('Id')},
-                        {field: 'weigh', title: __('Weigh')},
                         {field: 'title', title: __('名称')},
                         {field: 'bannername.name', title: __('Bannername.name')},
                         {field: 'img', title: __('Img'),formatter:formart_img},
@@ -35,9 +34,8 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                         {field: 'url', title: __('Url'), formatter: Table.api.formatter.url},
                         {field: 'url_type', title: __('Url_type'), searchList: {"内链":__('内链'),"外链":__('外链')}, formatter: Table.api.formatter.normal},
                         {field: 'status', title: __('Status'), searchList: {"显示":__('显示'),"隐藏":__('隐藏')}, formatter: Table.api.formatter.status},
-
                         {field: 'article.title', title: __('Article.title')},
-
+                        {field: 'weigh', title: __('排序')},
                         {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
                     ]
                 ]
