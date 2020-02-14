b4347deff28d76b1b9c9e7442b587c663f139ba2
wangjinhong <392513170@qq.com>
2020-02-14 18:59:57 +0800

    1

--- application/api/controller/Project.php
+++ application/api/controller/Project.php
@@ -34,7 +34,7 @@ class Project extends Api
         }
         $data=[];
 
-        $data["rows"]=(new \app\admin\model\Project())->where($where)->limit($offset,$page_size)->order("id","desc")->select();
+        $data["rows"]=(new \app\admin\model\Project())->where($where)->limit($offset,$page_size)->order("hot","desc")->select();
         $data["count"]=(new \app\admin\model\Project())->where($where)->count();
 
         $data["page"]=$page;
@@ -51,7 +51,6 @@ class Project extends Api
 
         try{
             $model=new \app\admin\model\Project();
-            $user = $this->auth->getUser();
             $id=$this->request->request('id');
 
             if(!$id){
@@ -72,5 +71,33 @@ class Project extends Api
 
     }
 
+
+    /*
+*更新项目阅读
+* **/
+    public function update()
+    {
+
+        try{
+            $model=new \app\admin\model\Project();
+            $id=$this->request->request('id');
+
+            if(!$id){
+                return $this->error(__('参数存在空'));
+            }
+            if(!$model::getById($id)){
+                return $this->error(__('项目不存在'));
+            }
+            $info=$model::getById($id);
+            $info->hot=$info->hot+1;
+            $info->rank=$info->rank+1;
+//            $model->save(['hot'=>$info->hot,'rank'=>$info->rank],['id'=>$id]);
+            $info->save();
+            return $this->success("成功");
+        }catch (Exception $e){
+            return  $this->error($e->getMessage());
+        }
+
+    }
 
 }
