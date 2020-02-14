2131ebd1ddd323149ccf2c21541557efcf94bb05
wangjinhong <392513170@qq.com>
2020-02-14 16:41:24 +0800

    1

--- public/assets/js/backend/banner.js
+++ public/assets/js/backend/banner.js
@@ -14,36 +14,50 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                 }
             });
 
-            var bannerList="{$bannerList}";
             var table = $("#table");
 
-            // 初始化表格
-            table.bootstrapTable({
-                url: $.fn.bootstrapTable.defaults.extend.index_url,
-                pk: 'id',
-                sortName: 'weigh',
-                columns: [
-                    [
-                        {checkbox: true},
-                        {field: 'id', title: __('Id')},
-                        {field: 'title', title: __('名称')},
-                        {field: 'bannername.name', title: __('Bannername.name'),searchList: JSON.parse(bannerList)},
-                        {field: 'img', title: __('Img'),formatter:formart_img},
-                        {field: 'top', title: __('Top'), searchList: {"置顶":__('置顶'),"取消置顶":__('取消置顶')}, formatter: Table.api.formatter.normal},
-                        {field: 'begin_time', title: __('Begin_time'), operate:'RANGE', addclass:'datetimerange', formatter: Table.api.formatter.datetime},
-                        {field: 'end_time', title: __('End_time'), operate:'RANGE', addclass:'datetimerange', formatter: Table.api.formatter.datetime},
-                        {field: 'url', title: __('Url'), formatter: Table.api.formatter.url},
-                        {field: 'url_type', title: __('Url_type'), searchList: {"内链":__('内链'),"外链":__('外链')}, formatter: Table.api.formatter.normal},
-                        {field: 'status', title: __('Status'), searchList: {"显示":__('显示'),"隐藏":__('隐藏')}, formatter: Table.api.formatter.status},
-                        {field: 'article.title', title: __('Article.title')},
-                        {field: 'weigh', title: __('排序')},
-                        {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
+
+            Fast.api.ajax({
+                url:"/admin.php/bannername/lists",
+                data:{}
+            }, function(data, ret){
+
+                console.log(data);
+                // 初始化表格
+                table.bootstrapTable({
+                    url: $.fn.bootstrapTable.defaults.extend.index_url,
+                    pk: 'id',
+                    sortName: 'weigh',
+                    columns: [
+                        [
+                            {checkbox: true},
+                            {field: 'id', title: __('Id')},
+                            {field: 'title', title: __('名称')},
+                            {field: 'bannername.name', title: __('Bannername.name'),searchList: JSON.parse(bannerList)},
+                            {field: 'img', title: __('Img'),formatter:formart_img},
+                            {field: 'top', title: __('Top'), searchList: {"置顶":__('置顶'),"取消置顶":__('取消置顶')}, formatter: Table.api.formatter.normal},
+                            {field: 'begin_time', title: __('Begin_time'), operate:'RANGE', addclass:'datetimerange', formatter: Table.api.formatter.datetime},
+                            {field: 'end_time', title: __('End_time'), operate:'RANGE', addclass:'datetimerange', formatter: Table.api.formatter.datetime},
+                            {field: 'url', title: __('Url'), formatter: Table.api.formatter.url},
+                            {field: 'url_type', title: __('Url_type'), searchList: {"内链":__('内链'),"外链":__('外链')}, formatter: Table.api.formatter.normal},
+                            {field: 'status', title: __('Status'), searchList: {"显示":__('显示'),"隐藏":__('隐藏')}, formatter: Table.api.formatter.status},
+                            {field: 'article.title', title: __('Article.title')},
+                            {field: 'weigh', title: __('排序')},
+                            {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
+                        ]
                     ]
-                ]
+                });
+
+                // 为表格绑定事件
+                Table.api.bindevent(table);
+
+            }, function(data, ret){
+
+                return false;
             });
 
-            // 为表格绑定事件
-            Table.api.bindevent(table);
+
+
         },
         add: function () {
             Controller.api.bindevent();
