3925e7f13077b295d64a6affe796b4786adfe6dc
wangjinhong <392513170@qq.com>
2020-02-14 19:02:16 +0800

    1

--- application/api/controller/Project.php
+++ application/api/controller/Project.php
@@ -91,7 +91,6 @@ class Project extends Api
             $info=$model::getById($id);
             $info->hot=$info->hot+1;
             $info->rank=$info->rank+1;
-//            $model->save(['hot'=>$info->hot,'rank'=>$info->rank],['id'=>$id]);
             $info->save();
             return $this->success("成功");
         }catch (Exception $e){
