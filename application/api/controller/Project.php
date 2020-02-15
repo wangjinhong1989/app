c5d3f3bf067a18fd5b42420b2de3639efafe2fd7
wangjinhong <392513170@qq.com>
2020-02-15 17:15:30 +0800

    1

--- application/api/controller/Project.php
+++ application/api/controller/Project.php
@@ -37,6 +37,9 @@ class Project extends Api
         $data["rows"]=(new \app\admin\model\Project())->where($where)->limit($offset,$page_size)->order("hot","desc")->select();
         $data["count"]=(new \app\admin\model\Project())->where($where)->count();
 
+        foreach ($data["rows"] as $k=>&$v){
+            $v["rank"]=($data["count"]-(($page-1)*$page_size-1));
+        }
         $data["page"]=$page;
 
         $data["total_page"]=ceil($data["count"]/$page_size);
@@ -61,8 +64,9 @@ class Project extends Api
             }
             $info=$model::getById($id);
             $info->hot=$info->hot+1;
-            $info->rank=$info->rank+1;
-//            $model->save(['hot'=>$info->hot,'rank'=>$info->rank],['id'=>$id]);
+
+ //          $info->rank=$info->rank+1;
+//           $model->save(['hot'=>$info->hot,'rank'=>$info->rank],['id'=>$id]);
             $info->save();
             return $this->success("成功",$info);
         }catch (Exception $e){
