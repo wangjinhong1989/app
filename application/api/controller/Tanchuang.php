9f26811e22a21e2ae566db1ccc347547bf74ce60
wangjinhong <392513170@qq.com>
2020-02-17 11:09:51 +0800

    1

--- application/api/controller/Tanchuang.php
+++ application/api/controller/Tanchuang.php
@@ -29,7 +29,6 @@ class Tanchuang extends Api
 
         $lists=(new \app\admin\model\Tanchuang())->where($where)->find();
 
-        dd($lists);
         if($lists){
 
             $temp=explode(",",$lists["image"]);
@@ -40,6 +39,47 @@ class Tanchuang extends Api
         }
         $this->success("成功",$lists);
     }
+
+
+    /**
+     * 首页
+     *
+     */
+    public function Lists()
+    {
+        $page=$this->request->request("page",1);
+        $page_size=$this->request->request("page_size",5);
+        $offset=($page-1)*$page_size;
+        $where=$data=[];
+        $where["status"]=["eq","显示"];
+        $url_type=$this->request->request("url_type","");
+        if($url_type){
+            $where["url_type"]=["eq",$url_type];
+        }
+
+
+        $model=(new \app\admin\model\Tanchuang());
+        $lists=$model->where($where)->order("paixu","asc")->orderRaw("rand()")->limit($offset,$page_size)->select();
+        $count=$model->where($where)->count();
+
+
+        foreach ($lists as &$l){
+            $temp=explode(",",$l["image"]);
+            foreach ($temp as &$t){
+                $t="http://app.biyouliao8.com".$t;
+            }
+            $l["images"]=$temp;
+
+        }
+        $data["page"]=$page;
+        $data["rows"]=$lists;
+        $data["count"]=$count;
+
+        $data["total_page"]=ceil($data["count"]/$page_size);
+
+        $this->success("成功",$lists);
+    }
+
 
 
 }
