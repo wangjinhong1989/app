726f44fd9f2aea81a48afe2e32664bc7b3506e9a
wjh <392513170@qq.com>
2020-02-24 17:14:22 +0800

    1

---
+++ application/admin/lang/zh-cn/client_log.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'Id'          => '编号',
+    'User_id'     => '用户',
+    'Content'     => '日志内容',
+    'Create_time' => '创建时间',
+    'Type'        => '类型'
+];
---
+++ public/assets/js/backend/client_log.js
@@ -0,0 +1,52 @@
+define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefined, Backend, Table, Form) {
+
+    var Controller = {
+        index: function () {
+            // 初始化表格参数配置
+            Table.api.init({
+                extend: {
+                    index_url: 'client_log/index' + location.search,
+                    add_url: 'client_log/add',
+                    edit_url: 'client_log/edit',
+                    del_url: 'client_log/del',
+                    multi_url: 'client_log/multi',
+                    table: 'client_log',
+                }
+            });
+
+            var table = $("#table");
+
+            // 初始化表格
+            table.bootstrapTable({
+                url: $.fn.bootstrapTable.defaults.extend.index_url,
+                pk: 'id',
+                sortName: 'id',
+                columns: [
+                    [
+                        {checkbox: true},
+                        {field: 'id', title: __('Id')},
+                        {field: 'user_id', title: __('User_id')},
+                        {field: 'create_time', title: __('Create_time'), operate:'RANGE', addclass:'datetimerange', formatter: Table.api.formatter.datetime},
+                        {field: 'type', title: __('Type')},
+                        {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
+                    ]
+                ]
+            });
+
+            // 为表格绑定事件
+            Table.api.bindevent(table);
+        },
+        add: function () {
+            Controller.api.bindevent();
+        },
+        edit: function () {
+            Controller.api.bindevent();
+        },
+        api: {
+            bindevent: function () {
+                Form.api.bindevent($("form[role=form]"));
+            }
+        }
+    };
+    return Controller;
+});
