3e562c1b86f01ce3586395c1664fea0081afd640
wangjinhong <392513170@qq.com>
2020-02-26 17:33:42 +0800

    1

--- application/api/controller/Tanchuang.php
+++ application/api/controller/Tanchuang.php
@@ -25,7 +25,8 @@ class Tanchuang extends Api
     {
         dd("test");
         dd($this->auth->id);
-
+        $time=Session::get("tanchuang".$this->auth->id);
+        dd($time);
         if(!$this->auth->id){
             $time=Session::get("tanchuang".$this->auth->id);
             $time=intval($time);
