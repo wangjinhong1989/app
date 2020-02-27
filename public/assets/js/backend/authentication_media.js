7ad574fa35d21b24211d7fa705065b98ee5dd71b
春上春樹 <392513170@qq.com>
2020-02-27 15:22:59 +0800

    1

--- public/assets/js/backend/authentication_media.js
+++ public/assets/js/backend/authentication_media.js
@@ -24,7 +24,7 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                 columns: [
                     [
                         {checkbox: true},
-                        {field: 'certificates_type', title: __('Certificates_type')},
+                        {field: 'id', title: __('Id')},
                         {field: 'status', title: __('Status'), searchList: {"审核中":__('审核中'),"审核通过":__('审核通过'),"审核不通过":__('审核不通过')}, formatter: Table.api.formatter.status},
                         {field: 'name', title: __('Name')},
                         {field: 'certificates_number', title: __('Certificates_number')},
