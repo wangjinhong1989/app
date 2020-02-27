f86dc93724ee306d90e4b8589e080ee4dbfb84b1
春上春樹 <392513170@qq.com>
2020-02-27 17:43:05 +0800

    1

--- public/assets/js/backend/authentication_enterprise.js
+++ public/assets/js/backend/authentication_enterprise.js
@@ -28,7 +28,9 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                         {field: 'user.username', title: __('User.username')},
                         {field: 'certificates_type', title: __('Certificates_type')},
                         {field: 'status', title: __('Status'), searchList: {"审核中":__('审核中'),"审核通过":__('审核通过'),"审核不通过":__('审核不通过')}, formatter: Table.api.formatter.status},
-                        {field: 'personal_info.status', title: __('运营者审核状态'),formatter:fmt},
+                        {field: 'personal_info.status', title: __('运营者审核状态'),
+                            classname: 'btn btn-xs btn-primary btn-dialog', url: 'authentication_personal/edit?id='+personal_info.id
+                        },
                         {field: 'name', title: __('Name')},
                         {field: 'certificates_number', title: __('Certificates_number')},
                         {field: 'files', title: __('Files'),formatter:formart_img},
@@ -56,15 +58,15 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
         }
     };
 
-    function fmt(value,row){
-
-        var str="";
-        if(row["personal_info"]["status"]!=undefined)
-
-         return   $str="<a target='_blank' href=\"/admin.php/authentication_personal/edit?id="+row["personal_info"]["id"]+"'\" title=\"{:__('Edit')}\" ><i class=\"fa fa-pencil\"></i> "+row["personal_info"]["status"]+"</a>"
-
-        else
-            return "-";
-    }
+    // function fmt(value,row){
+    //
+    //     var str="";
+    //     if(row["personal_info"]["status"]!=undefined)
+    //
+    //      return   $str="<a target='_blank' href=\"/admin.php/authentication_personal/edit?id="+row["personal_info"]["id"]+"'\" title=\"{:__('Edit')}\" ><i class=\"fa fa-pencil\"></i> "+row["personal_info"]["status"]+"</a>"
+    //
+    //     else
+    //         return "-";
+    // }
     return Controller;
 });
