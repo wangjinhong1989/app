84fd09a31c21efd11804b3dcc90d9a69a093a503
春上春樹 <392513170@qq.com>
2020-02-27 16:54:55 +0800

    1

--- public/assets/js/backend/authentication_enterprise.js
+++ public/assets/js/backend/authentication_enterprise.js
@@ -25,6 +25,7 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                     [
                         {checkbox: true},
                         {field: 'id', title: __('Id')},
+                        {field: 'user.username', title: __('User.username')},
                         {field: 'certificates_type', title: __('Certificates_type')},
                         {field: 'status', title: __('Status'), searchList: {"审核中":__('审核中'),"审核通过":__('审核通过'),"审核不通过":__('审核不通过')}, formatter: Table.api.formatter.status},
                         {field: 'status_user', title: __('运营者审核'), searchList: {"审核中":__('审核中'),"审核通过":__('审核通过'),"审核不通过":__('审核不通过')}},
@@ -33,7 +34,7 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                         {field: 'files', title: __('Files'),formatter:formart_img},
                         {field: 'note', title: __('Note')},
                         {field: 'time', title: __('Time'), operate:'RANGE', addclass:'datetimerange', formatter: Table.api.formatter.datetime},
-                        {field: 'user.username', title: __('User.username')},
+
                         {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
                     ]
                 ]
