33f2d31cf44bd4e6e7f56289204f84486d11eb88
春上春樹 <392513170@qq.com>
2020-03-01 20:58:15 +0800

    1

--- public/assets/js/backend/banner_detail.js
+++ public/assets/js/backend/banner_detail.js
@@ -30,10 +30,12 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                         {field: 'title', title: __('广告名称')},
                         {field: 'weigh', title: __('排序')},
                         {field: 'img', title: __('图片'),formatter:formart_img},
-                           {field: 'url', title: __('链接地址'), formatter: Table.api.formatter.url},
-                        {field: 'url_type', title: __('链接类型'), searchList: {"内链":__('内链'),"外链":__('外链')}, formatter: Table.api.formatter.normal},
                         {field: 'status', title: __('状态'), searchList: {"显示":__('显示'),"隐藏":__('隐藏')}, formatter: Table.api.formatter.status},
-                        {field: 'begin_time', title: __('开始时间'), operate:'RANGE', addclass:'datetimerange', formatter: Table.api.formatter.datetime},
+
+
+                        {field: 'url_type', title: __('链接类型'), searchList: {"内链":__('内链'),"外链":__('外链')}, formatter: Table.api.formatter.normal},
+                        {field: 'url', title: __('链接地址'), formatter: Table.api.formatter.url},
+                         {field: 'begin_time', title: __('开始时间'), operate:'RANGE', addclass:'datetimerange', formatter: Table.api.formatter.datetime},
                         {field: 'end_time', title: __('结束时间'), operate:'RANGE', addclass:'datetimerange', formatter: Table.api.formatter.datetime},
 
                         {field: 'article.title', title: __('文章标题')},
