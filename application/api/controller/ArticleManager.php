1c8673ff621e0eec85995abad4eada42fa5576d7
春上春樹 <392513170@qq.com>
2020-03-05 22:32:15 +0800

    10tiao

--- application/api/controller/ArticleManager.php
+++ application/api/controller/ArticleManager.php
@@ -29,7 +29,7 @@ class ArticleManager extends Api
     public function Lists()
     {
         $page=$this->request->request("page",1);
-        $page_size=$this->request->request("page_size",5);
+        $page_size=$this->request->request("page_size",10);
         $offset=($page-1)*$page_size;
 
         if($offset<0){
