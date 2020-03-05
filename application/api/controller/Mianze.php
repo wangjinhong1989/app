ebd0fe206c21de7724f10c0f6868f7c2c1f2386d
春上春樹 <392513170@qq.com>
2020-03-05 22:39:11 +0800

    1

---
+++ application/api/controller/Mianze.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace app\api\controller;
+
+use app\common\controller\Api;
+use app\admin\model\Guanggao;
+use think\Cache;
+use think\Config;
+use think\db\Query;
+use think\Session;
+
+/**
+ * 首页接口
+ */
+class Mianze extends Api
+{
+    protected $noNeedLogin = ['*'];
+    protected $noNeedRight = ['*'];
+
+
+    /**
+     * 首页
+     *
+     */
+    public function wenzhang()
+    {
+
+        $model= new Query();
+        $lists= $model->table("fa_mianze1")->where([])->select();
+       return $this->success("成功",$lists);
+    }
+
+
+    /**
+     * 首页
+     *
+     */
+    public function tuijian()
+    {
+
+        $model= new Query();
+        $lists= $model->table("fa_mianze2")->where([])->select();
+        return $this->success("成功",$lists);
+    }
+
+
+}
