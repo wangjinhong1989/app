e540353efa373279f384d827656aa6874e587629
春上春樹 <392513170@qq.com>
2020-03-06 00:20:04 +0800

    1

--- application/api/controller/ArticleManager.php
+++ application/api/controller/ArticleManager.php
@@ -797,7 +797,7 @@ span.s2 {font-family: \'Helvetica\'; font-weight: normal; font-style: normal; fo
             ];
             $pushModel->create($temp);
 
-            Cache::set("add_article_number".$this->auth->id.date("Ymd",time()),$my_number++,60);
+            Cache::set("add_article_number".$this->auth->id.date("Ymd",time()),$my_number++,660);
 
             return $this->success();
         }catch (Exception $e){
