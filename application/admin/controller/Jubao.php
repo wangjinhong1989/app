e3ac717319ef379594fd7ffa16349b5d67d6105f
春上春樹 <392513170@qq.com>
2020-03-08 16:24:11 +0800

    1

--- application/admin/controller/Jubao.php
+++ application/admin/controller/Jubao.php
@@ -88,4 +88,77 @@ class Jubao extends Backend
         }
         return $this->view->fetch();
     }
+
+
+    /**
+     * 编辑
+     */
+    public function edit($ids = null)
+    {
+        $row = $this->model->get($ids);
+        if (!$row) {
+            $this->error(__('No Results were found'));
+        }
+
+
+        $adminIds = $this->getDataLimitAdminIds();
+        if (is_array($adminIds)) {
+            if (!in_array($row[$this->dataLimitField], $adminIds)) {
+                $this->error(__('You have no permission'));
+            }
+        }
+        if ($this->request->isPost()) {
+            $params = $this->request->post("row/a");
+            if ($params) {
+
+                $params = $this->preExcludeFields($params);
+                $result = false;
+
+                Db::startTrans();
+                try {
+                    //是否采用模型验证
+                    if ($this->modelValidate) {
+                        $name = str_replace("\\model\\", "\\validate\\", get_class($this->model));
+                        $validate = is_bool($this->modelValidate) ? ($this->modelSceneValidate ? $name . '.edit' : $name) : $this->modelValidate;
+                        $row->validateFailException(true)->validate($validate);
+                    }
+
+
+                    $result = $row->allowField(true)->save($params);
+
+                    if($params["status"]=="有效"){
+                        (new \app\admin\model\Article())->where(["id"=>$params["article_id"]])->delete();
+                    }
+                    Db::commit();
+                    dd($result);
+                } catch (ValidateException $e) {
+                    Db::rollback();
+                    $this->error($e->getMessage());
+                } catch (PDOException $e) {
+                    Db::rollback();
+                    $this->error($e->getMessage());
+                } catch (Exception $e) {
+                    Db::rollback();
+                    $this->error($e->getMessage());
+                }
+                if ($result !== false) {
+                    $this->success();
+                } else {
+                    $this->error(__('No rows were updated'));
+                }
+            }
+            $this->error(__('Parameter %s can not be empty', ''));
+        }
+
+
+        // 文章信息.
+
+        $article= (new \app\admin\model\Article())->where(["id"=>$row["article_id"]])->field("content")->find();
+
+        $row["article_content"]=!empty($article)?$article[0]["content"]:"";
+        $this->view->assign("row", $row);
+        return $this->view->fetch();
+    }
+
+
 }
