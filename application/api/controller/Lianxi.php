a642a6a72c131d6ef859d54e7b054a6b06ca9cd2
春上春樹 <392513170@qq.com>
2020-03-08 22:06:41 +0800

    1

---
+++ application/api/controller/Lianxi.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace app\api\controller;
+
+use app\common\controller\Api;
+use app\admin\model\Guanggao;
+use think\Cache;
+use think\Config;
+use think\db\Query;
+
+/**
+ * 首页接口
+ */
+class Lianxi extends Api
+{
+    protected $noNeedLogin = ['*'];
+    protected $noNeedRight = ['*'];
+
+    /**
+     * 首页
+     *
+     */
+    public function Lists()
+    {
+        $model=    new Query();
+        $lists=$model->table("fa_lianxi")->where(["status"=>"显示"])->order("paixu","asc")->select();
+        $this->success("成功",$lists);
+    }
+}
