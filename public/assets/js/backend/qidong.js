99a85c6c67cb5f7202912e27bc42f2c82c7f9380
春上春樹 <392513170@qq.com>
2020-03-08 22:24:21 +0800

    1

--- public/assets/js/backend/qidong.js
+++ public/assets/js/backend/qidong.js
@@ -34,7 +34,7 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                         {field: 'status', title: __('Status'), searchList: {"显示":__('显示'),"隐藏":__('隐藏')}, formatter: Table.api.formatter.status},
                         {field: 'begin_time', title: __('Begin_time'), operate:'RANGE', addclass:'datetimerange', formatter: Table.api.formatter.datetime},
                         {field: 'end_time', title: __('End_time'), operate:'RANGE', addclass:'datetimerange', formatter: Table.api.formatter.datetime},
-                        {field: 'article.title', title: __('Article.title')},
+                        {field: 'article.title', title: __('文章标题')},
                         {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
                     ]
                 ]
