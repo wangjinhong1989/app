722e02080371deccbdd154710a27df322ded3a73
春上春樹 <392513170@qq.com>
2020-03-08 22:18:56 +0800

    1

--- public/assets/js/backend/tanchuang.js
+++ public/assets/js/backend/tanchuang.js
@@ -34,7 +34,7 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                         {field: 'url_type', title: __('Url_type'), searchList: {"内链":__('内链'),"外链":__('外链')}, formatter: Table.api.formatter.normal},
                         {field: 'url', title: __('Url'), formatter: Table.api.formatter.url},
                         {field: 'status', title: __('Status'), searchList: {"显示":__('显示'),"隐藏":__('隐藏')}, formatter: Table.api.formatter.status},
-                        {field: 'article.title', title: __('文章')},
+                        {field: 'article.title', title: __('文章标题')},
                         {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
                     ]
                 ]
