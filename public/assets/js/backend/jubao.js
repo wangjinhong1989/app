8cd0ad2df3af2028478945fc601fdfd158e9e5c9
春上春樹 <392513170@qq.com>
2020-03-08 16:42:09 +0800

    1

--- public/assets/js/backend/jubao.js
+++ public/assets/js/backend/jubao.js
@@ -30,7 +30,9 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
                         {field: 'username', title: __('举报人')},
                         {field: 'type', title: __('Type'), searchList: {"内容抄袭或转载":__('内容抄袭或转载'),"广告或垃圾信息":__('广告或垃圾信息'),"其它":__('其它')}, formatter: Table.api.formatter.normal},
                         {field: 'status', title: __('Status'), searchList: {"有效":__('有效'),"无效":__('无效'),"审核":__('审核')}, formatter: Table.api.formatter.status},
-                        {field: 'content', title: __('Content')},
+                        {field: 'content', title: __('Content'),formatter: function (value) {
+                                return value.substr(0, 50);
+                            }},
 
                         {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
                     ]
