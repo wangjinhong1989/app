5dac85789bee170a465ef8f65b5f0b9de4436b38
春上春樹 <392513170@qq.com>
2020-03-08 19:14:35 +0800

    1

--- application/admin/model/Tanchuang.php
+++ application/admin/model/Tanchuang.php
@@ -109,6 +109,8 @@ class Tanchuang extends Model
         $where["end_time"]=["egt",time()];
         if(!empty($notIn)){
             $lists=self::where($where)->order("paixu","asc")->orderRaw("rand()")->whereNotIn("id",$notIn)->limit(1,1)->select();
+
+            echo self::getLastSql();
         }else {
             $lists=self::where($where)->order("paixu","asc")->orderRaw("rand()")->limit(1,1)->select();
         }
