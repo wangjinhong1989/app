2316dfb1dbc1e453cc4b0624ef0cc14e1ff47a72
wjh <392513170@qq.com>
2020-03-09 21:27:16 +0800

    1

---
+++ application/admin/lang/zh-cn/subject_new.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'Id'                     => '编号',
+    'Article_id'             => '文章',
+    'Paixu'                  => '排序',
+    'Create_time'            => '创建时间',
+    'Article.title'          => '标题',
+    'Article.status'         => '有效无效',
+    'Article.articletype_id' => '文章类型',
+    'Article.img'            => '封面'
+];
