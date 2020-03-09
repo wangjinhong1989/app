5bb83da82cab0e0afe5028be6e0179653636ee43
春上春樹 <392513170@qq.com>
2020-03-09 22:28:35 +0800

    1

---
+++ application/api/controller/Daodu.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace app\api\controller;
+
+use app\common\controller\Api;
+use app\admin\model\Guanggao;
+use think\Cache;
+use think\Config;
+use think\db\Query;
+
+/**
+ * 首页接口
+ */
+class Daodu extends Api
+{
+    protected $noNeedLogin = ['*'];
+    protected $noNeedRight = ['*'];
+
+    /**
+     * 首页
+     *
+     */
+    public function Lists()
+    {
+        $model=    new Query();
+        $lists=$model->table("fa_guanggao")->where(['status'=>'显示'])->orderRaw("rand()")->limit(0,1)->select();
+        $this->success("成功",$lists);
+    }
+}
