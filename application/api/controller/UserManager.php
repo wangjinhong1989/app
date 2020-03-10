1f9b144b5574ded26d9fa83fa642dc5628cb34bd
春上春樹 <392513170@qq.com>
2020-03-10 22:11:14 +0800

    1

--- application/api/controller/UserManager.php
+++ application/api/controller/UserManager.php
@@ -122,7 +122,7 @@ class UserManager extends Api
     public function back_lists()
     {
         $page=$this->request->request("page",1);
-        $page_size=$this->request->request("page_size",100);
+        $page_size=100;//$this->request->request("page_size",100);
         $offset=($page-1)*$page_size;
 
         if($offset<0){
