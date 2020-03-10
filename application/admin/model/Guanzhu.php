b7370d84abb5b8c56d4616e8285ddcee1fd168af
春上春樹 <392513170@qq.com>
2020-03-11 00:06:56 +0800

    1

--- application/admin/model/Guanzhu.php
+++ application/admin/model/Guanzhu.php
@@ -52,6 +52,7 @@ class Guanzhu extends Model
 
     public function initUser($user_id){
         $data=self::where([])->select();
+        var_dump($data);
         foreach ($data as $v){
             (new Guanzhu())->create([
                 "user_id"=>$user_id,
