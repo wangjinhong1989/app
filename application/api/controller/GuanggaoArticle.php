6363e51bd759fa3155b077bb9be9e7220d86d491
春上春樹 <392513170@qq.com>
2020-03-10 23:40:30 +0800

    1

--- application/api/controller/GuanggaoArticle.php
+++ application/api/controller/GuanggaoArticle.php
@@ -37,8 +37,9 @@ class GuanggaoArticle extends Api
 
         $query=new Query();
         $data["rows"]=$query->table("fa_guanggao_article")->alias("subject")->where($where)
-            ->field("article.*")
+            ->field("article.*,user.username,user.avatar")
             ->join("fa_article article","article.id=subject.article_id","left")
+            ->join("fa_user user","user.id=subject.user_id","left")
             ->order($sort ,$order) ->limit($offset,$page_size)->select();
 
 
@@ -46,6 +47,7 @@ class GuanggaoArticle extends Api
 
 
         foreach ($data["rows"] as $key=>&$value){
+            $value["create_time"]=formart_time($value["create_time"]);
             $value["create_time"]=formart_time($value["create_time"]);
             $value["is_ad"]=false;
         }
