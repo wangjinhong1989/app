7eacc95be5931299c4ca613a388ac508b32d01a4
春上春樹 <392513170@qq.com>
2020-03-12 00:45:23 +0800

    1

--- application/common/controller/Backend.php
+++ application/common/controller/Backend.php
@@ -475,6 +475,7 @@ class Backend extends Controller
         }
         $list = [];
         $total = $this->model->where($where)->count();
+        echo $this->model->getLastSql();
         if ($total > 0) {
             if (is_array($adminIds)) {
                 $this->model->where($this->dataLimitField, 'in', $adminIds);
