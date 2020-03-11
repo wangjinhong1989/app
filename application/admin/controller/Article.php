6972fa6fdfcc5011adae807055ab657caf98a0b1
春上春樹 <392513170@qq.com>
2020-03-11 23:48:23 +0800

    1

--- application/admin/controller/Article.php
+++ application/admin/controller/Article.php
@@ -55,7 +55,7 @@ class Article extends Backend
             }
             list($where, $sort, $order, $offset, $limit) = $this->buildparams("title,user.username,id");
 
-            var_dump($where);
+
             $total = $this->model
                     ->with(['articletype','label','user'])
                     ->where($where)
