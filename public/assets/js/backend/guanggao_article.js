5ffcdc77fa185ec8c991f2ab146bdefd08d22f31
春上春樹 <392513170@qq.com>
2020-03-12 00:49:19 +0800

    1

--- public/assets/js/backend/guanggao_article.js
+++ public/assets/js/backend/guanggao_article.js
@@ -38,12 +38,15 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
         },
         add: function () {
                 $("#c-article_id").data("params", function (obj) {
-                    return {custom: {articletype_id: ["NOT IN",2]}} //{filter: {"articletype.name":"快讯,关注"},op: {"articletype.name":"NOT IN"}};
+                    return {custom: {articletype_id: ["NOT IN",2]}}
                 });
                 Controller.api.bindevent();
 
         },
         edit: function () {
+            $("#c-article_id").data("params", function (obj) {
+                return {custom: {articletype_id: ["NOT IN",2]}}
+            });
             Controller.api.bindevent();
         },
         api: {
