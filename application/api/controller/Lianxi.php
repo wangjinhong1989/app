9a5e876ee3700487fe2b44df69ae5ec5749fe278
春上春樹 <392513170@qq.com>
2020-03-21 19:06:30 +0800

    1

--- application/api/controller/Lianxi.php
+++ application/api/controller/Lianxi.php
@@ -23,7 +23,7 @@ class Lianxi extends Api
     public function Lists()
     {
         $model=    new Query();
-        $lists=$model->table("fa_lianxi")->where([])->order("paixu","asc")->select();
+        $lists=$model->table("fa_lianxi")->where([])->order("paixu","desc")->select();
         $this->success("成功",$lists);
     }
 }
