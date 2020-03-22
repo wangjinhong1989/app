bbe544e4765719d11da68bdfbfd57314095647dc
春上春樹 <392513170@qq.com>
2020-03-22 21:00:54 +0800

    1

--- application/api/controller/ArticleManager.php
+++ application/api/controller/ArticleManager.php
@@ -986,7 +986,7 @@ span.s2 {font-family: \'Helvetica\'; font-weight: normal; font-style: normal; fo
             $my_number=$my_number+1;
             Cache::set("add_article_number".$this->auth->id.date("Ymd",time()),$my_number,660);
 
-            return \GuzzleHttp\json_encode([
+            echo \GuzzleHttp\json_encode([
                "msg"=>"",
                "data"=>null,
                 "code"=>1,
@@ -994,7 +994,7 @@ span.s2 {font-family: \'Helvetica\'; font-weight: normal; font-style: normal; fo
                 "url"=> "/index/article/index",
                 "wait"=> 3
             ]);
-            return $this->success();
+            //return $this->success();
 
         }catch (Exception $e){
             return  $this->error($e->getMessage());
