ad043432a1aa30f7a29be3d88859f9f127287963
wangjinhong1989 <392513170@qq.com>
2020-03-29 18:04:52 +0800

    1

--- application/common/library/Auth.php
+++ application/common/library/Auth.php
@@ -2,6 +2,7 @@
 
 namespace app\common\library;
 
+use app\admin\model\Avatar;
 use app\admin\model\GuangfangUser;
 use app\admin\model\Guanggao;
 use app\api\controller\Guanzhu;
@@ -10,6 +11,7 @@ use app\common\model\UserRule;
 use fast\Random;
 use think\Config;
 use think\Db;
+use think\db\Query;
 use think\Exception;
 use think\Hook;
 use think\Request;
@@ -166,6 +168,13 @@ class Auth
         ]);
 
         if($params["avatar"]==""){
+
+            //
+            $modelQ=    new Query();
+            $dataQ=$modelQ->table("fa_avatar")->where([])->orderRaw("rand()")->limit(0,1)->select();
+            if($dataQ){
+                $params["avatar"]=$dataQ[0]["avatar"];
+            }else
             $params["avatar"]=Config::get('api_url')."/g.jpg";
         }
         $params['password'] = $this->getEncryptPassword($password, $params['salt']);
