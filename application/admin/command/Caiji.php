a2e95245796c4991d854e4f399abf4f19ff02f39
春上春樹 <392513170@qq.com>
2020-04-25 18:38:52 +0800

    1

--- application/admin/command/Caiji.php
+++ application/admin/command/Caiji.php
@@ -101,7 +101,7 @@ class Caiji extends Command
 
                     \app\admin\model\Article::create(
                         [
-                            "title"=>mb_strstr($v["content"],0,50,"utf-8"),
+                            "title"=>mb_strstr($v["content"],"】")."】",
                             "content"=>$v["content"],
                             "lk_count"=>$v["down_counts"],
                             "lh_count"=>$v["up_counts"],
