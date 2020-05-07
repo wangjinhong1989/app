14704e596758583b381f0a02af8cf632a1592d58
王金宏 <2709989114@qq.com>
2020-05-07 10:11:05 +0800

    xiugai

--- application/api/controller/ArticleManager.php
+++ application/api/controller/ArticleManager.php
@@ -1032,6 +1032,7 @@ span.s2 {font-family: \'Helvetica\'; font-weight: normal; font-style: normal; fo
             $detail["lihao_count"]=(new Lihaokong())->where(["article_id"=>$detail["id"],"is_profit"=>"利好"])->count();
 
             $detail["image"]=Config::get("site.快讯封面");
+            $detail["erweima"]=Config::get("site.快讯二维码");
             //$detail["url"]="http://app.xwxyq.cn/uploads/20191224/1c68375a95c34071687ca6a56f5f8933.png";
             //$detail["create_time"]=date("Y-m-d H:i:s",$detail["create_time"]);
             $detail["create_time"]=formart_time($detail["create_time"]);
