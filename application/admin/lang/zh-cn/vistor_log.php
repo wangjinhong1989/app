422ed1d7c6e5d11f5169d0194b6e024b48af6fa1
wjh <392513170@qq.com>
2020-05-25 14:29:59 +0800

    xiugai

---
+++ application/admin/lang/zh-cn/vistor_log.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'Id'            => 'ID',
+    'User_id'       => '用户ID',
+    'Page'          => '访问页面或接口',
+    'Ip'            => 'IP地址',
+    'Open_time'     => '打开时间',
+    'User.username' => '用户名'
+];
