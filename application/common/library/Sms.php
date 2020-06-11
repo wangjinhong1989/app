51303d22ef2017aef4a050cb7ef80fbfa1475d76
春上春樹 <392513170@qq.com>
2020-06-11 19:44:01 +0800

    xiug

--- application/common/library/Sms.php
+++ application/common/library/Sms.php
@@ -49,6 +49,7 @@ class Sms
      */
     public static function send($mobile, $code = null, $event = 'default')
     {
+        var_dump($mobile);
         $code = is_null($code) ? mt_rand(1000, 9999) : $code;
         $time = time();
         $ip = request()->ip();
