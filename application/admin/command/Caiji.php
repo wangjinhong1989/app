e497b91ab01474255cc99156c621d582e990afc2
王金宏 <2709989114@qq.com>
2020-06-11 10:30:42 +0800

    xiugai

--- application/admin/command/Caiji.php
+++ application/admin/command/Caiji.php
@@ -92,7 +92,7 @@ class Caiji extends Command
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
+        curl_setopt($ch, CURLOPT_TIMEOUT,20);
         $curlRes = curl_exec($ch);
 
         var_dump($curlRes);
